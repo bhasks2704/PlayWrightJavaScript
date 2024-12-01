c2c984746bb62d6eb10622ac36102d9cdc32c4dc
admin <bhaskarrao441@gmail.com>
2024-12-01 21:41:38 +0530

    changed  RadioButton

--- tests/Radiobutton.spec.js
+++ tests/Radiobutton.spec.js
@@ -5,8 +5,14 @@ test("radio button Test case", async ({page})=>{
     await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
 
     let radio1 = await page.locator("//label[@for='radio1']/input[@name='radioButton']")
+    let radio2 = await page.locator("//label[@for='radio2']/input[@name='radioButton']")
 
     radio1.check()
+
+    await expect(radio1).toBeChecked()
+    await expect(radio1.isChecked()).toBeTruthy()
+
+    // await expect(radio2.isChecked()).toBeFalsy()
 
     await page.waitForTimeout(3000)
 })
